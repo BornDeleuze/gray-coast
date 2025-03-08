797291a97f49c227f988225cb9df9a0a1a9e4fb6
Matt Beaudoin <ehxtwo@gmail.com>
2025-03-08 14:12:02 -0800

    move content to home page

--- src/App.jsx
+++ src/App.jsx
@@ -25,44 +25,6 @@ function App() {
                 <Route path="/get_involved" element={<GetInvolved />} />
               </Routes>
           </div>
-          <div>
-            <a href="/"
-            >
-              <img src="/images/sunset.jpg" class="relative text-center"/>
-              <h5 class="text-xlg top-100px absolute text-red-900"> Gray Coast Guildhall </h5>
-            </a>
-          </div>
-          <div>
-            <header>
-              {/* <h3 class="text-lg"> Gray Coast Guildhall </h3> */}
-            </header>
-            <h3>
-              Welcome
-            </h3>
-            <div>
-              The Gray Coast Guildhall is a space for gathering, creating, organizing, and performing. Our home is the Old Theater Building in Quilcene, Washington, on traditional Twana & S’Klallam territory.
-              We provide space for music events and other performances, meetings and gatherings, dances, movie nights, and more. We are working on creating space for local artists and crafters to set up small working studios.
-              The Grey Coast Guildhall is a not-for-profit project, and is collectively managed by a team of dedicated volunteers. This space is only possible with the help and support of many people, and we welcome you to get involved!
-            </div>
-            <img src="/public/images/back.jpg"/>
-          </div>
-          <div>
-            <div>
-              <h4>
-                Visit Us
-              </h4>
-              <h5>
-                11 Old church rd
-                Quilcene, WA  98376
-                USA
-              </h5>
-            </div>
-            <div>
-              <h4>
-                Send us a message
-              </h4>
-            </div>
-          </div>
         </div>
         </Router>
     </>
