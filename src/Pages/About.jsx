1c6311023d04c3fb16ece24d7dfd017d35f27cad
Matt Beaudoin <ehxtwo@gmail.com>
2025-03-21 11:21:34 -0700

    add basic copy to pages

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,14 @@
 
 const About = () => {
   return (
-     <div>
-        <h1>Hello about</h1>
-     </div>
+     <>
+         <div class="flex">
+            <h1>The Gray Coast Guildhall is a space for gathering, creating, organizing, and performing. Our home is the Old Theater Building in Quilcene, Washington, on traditional Twana & S’Klallam territory.
+               Gray Coast Guildhall holds space to generate and nourish radical and DIY arts, culture, organizing, and resistance on the Olympic Peninsula. We operate through collective decision making and mutual aid, and oppose all forms of oppression.
+            </h1>
+            {/* <img>ooops</img> */}
+         </div>
+     </>
   );
 };
 
