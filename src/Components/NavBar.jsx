9b21f7d1129face307e0c846b213fd37e7a1bd7d
Matt Beaudoin <ehxtwo@gmail.com>
2025-03-30 23:19:24 -0700

    hamburger working

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,38 +1,47 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-class NavBar extends React.Component {
-  componentDidMount() {
-    const menuToggle = document.getElementById("menu-toggle");
-    const menu = document.getElementById("menu");
+const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    menuToggle.addEventListener("click", () => {
-      menu.classList.toggle("hidden");
-    });
-  }
-
-  render() {
-    return (
-      <>
-        <nav className="px-8 py-2 bg-gray-900 text-gray-200">
-          <div className="flex justify-between items-center">
-            <button id="menu-toggle" className="md:hidden text-gray-200 focus:outline-none">
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-              </svg>
-            </button>
-          </div>
-          <ul id="menu" className="hidden md:flex justify-evenly uppercase mt-2 md:mt-0">
-            <li><a href="/" className="block py-2 px-4 hover:text-gray-400">Home</a></li>
-            <li><a href="/about" className="block py-2 px-4 hover:text-gray-400">About</a></li>
-            <li><a href="/events" className="block py-2 px-4 hover:text-gray-400">Events</a></li>
-            <li><a href="/donate" className="block py-2 px-4 hover:text-gray-400">Donate</a></li>
-            <li><a href="/get_involved" className="block py-2 px-4 hover:text-gray-400">Get Involved</a></li>
-          </ul>
-        </nav>
-      </>
-    );
-  }
-}
+  return (
+    <nav className="px-8 py-2 bg-gray-900 text-gray-200 relative z-50">
+      <div className="flex justify-between items-center relative">
+      <div className="h-9">
+      </div> 
+        <button
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="md:hidden text-gray-200 focus:outline-none absolute top-2 right-4 z-50"
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16m-7 6h7"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <ul
+        className={`${
+          menuOpen ? "block" : "hidden"
+        } absolute top-full right-0 bg-gray-900 w-full md:static md:flex justify-evenly uppercase mt-2 md:mt-0 z-50`}
+      >
+        <li><Link to="/" className="block py-2 px-4 hover:text-gray-400">Home</Link></li>
+        <li><Link to="/about" className="block py-2 px-4 hover:text-gray-400">About</Link></li>
+        <li><Link to="/events" className="block py-2 px-4 hover:text-gray-400">Events</Link></li>
+        <li><Link to="/donate" className="block py-2 px-4 hover:text-gray-400">Donate</Link></li>
+        <li><Link to="/get_involved" className="block py-2 px-4 hover:text-gray-400">Get Involved</Link></li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
