38f18225bfaa3cafac0b4396e6153df0bd23e304
Matt Beaudoin <ehxtwo@gmail.com>
2025-04-16 21:42:25 -0700

    style pass

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -83,7 +83,7 @@ const NavBar = () => {
           <div class="relative">
             <img src="/images/sunset.jpg" class="w-full" alt="Sunset"/>
             <div class="absolute inset-0"></div>
-            <h5 class="font-dmserif outline-5 absolute font-semibold tracking-wide text-3xl md:text-5xl text-black top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 uppercase text-center drop-shadow-[0_0_2px_#999999]">
+            <h5 class="font-dmserif outline-5 px-4 py-2 absolute font-semibold tracking-wide text-3xl md:text-5xl text-black top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 uppercase text-center drop-shadow-[0_0_2px_#999999]">
               Gray Coast Guildhall
             </h5>
           </div>
