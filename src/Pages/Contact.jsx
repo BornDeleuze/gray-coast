d96dc95fe60b27188ffd6c0b08ea629faea3cd16
Matt Beaudoin <ehxtwo@gmail.com>
2025-04-27 20:43:54 -0700

    fill out contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
 function Contact() {
 
   return (
     <>
+      <div className="px-4 py-8 max-w-4xl mx-auto">
+       <div className="space-y-4 text-lg text-gray-800 leading-relaxed">
+         <p>
+          The Guildhall would love to hear from you! If you would like to get in touch with our collective,
+          to inquire about private rentals, or to sign up for a monthly email newsletter, use the contact form below. 
+          For show inquiries, please reach out the the events team using the form on the 
+          <Link to="/events" className="hover:text-gray-400"> <strong>events page.</strong> </Link> 
+         </p>
+        </div>
+      </div>
+
       <div className="mt-10 flex justify-center">
         <div className="w-full max-w-md bg-gray-90 p-6 rounded-2xl shadow-lg border border-gray-200">
-          <h2 className="text-2xl font-bold mb-4 text-center text-gray-800">Send us a message</h2>
+          <h2 className="text-2xl font-bold mb-4 text-center text-gray-800">Send the collective a message</h2>
             <form
                action="https://formsubmit.co/6cf70ccd3364c6b0fbdd1583a2d652e9"
                method="POST"
